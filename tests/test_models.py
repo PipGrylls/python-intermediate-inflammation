57bc63c9c19f502871dee6157e3d63f607b6f06d
Pip Grylls <pipgryllsastro@gmail.com>
2021-04-26 16:31:26 +0100

    extending test suit

--- tests/test_models.py
+++ tests/test_models.py
@@ -30,4 +30,55 @@ def test_daily_mean_integers():
     npt.assert_array_equal(np.array([3, 4]), daily_mean(test_array))
 
 
+def test_daily_min_zeros():
+    """Test that min function works for an array of zeros."""
+    from inflammation.models import daily_min
+
+    # NB: the comment 'yapf: disable' disables automatic formatting using
+    # a tool called 'yapf' which we have used when creating this project
+    test_array = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([0, 0]), daily_min(test_array))
+
+
+def test_daily_min_integers():
+    """Test that min function works for an array of positive integers."""
+    from inflammation.models import daily_min
+
+    test_array = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([1, 2]), daily_min(test_array))
+
+
+def test_daily_max_zeros():
+    """Test that max function works for an array of zeros."""
+    from inflammation.models import daily_max
+
+    # NB: the comment 'yapf: disable' disables automatic formatting using
+    # a tool called 'yapf' which we have used when creating this project
+    test_array = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([0, 0]), daily_max(test_array))
+
+
+def test_daily_max_integers():
+    """Test that max function works for an array of positive integers."""
+    from inflammation.models import daily_max
+
+    test_array = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([5, 6]), daily_max(test_array))
+
 # TODO(lesson-robust) Implement tests for the other statistical functions
